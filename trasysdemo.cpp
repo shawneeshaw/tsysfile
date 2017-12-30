03eb513755420a0a8257dd4a1c02bcd8e65c1dad
shawneeshaw <shawneeshaw@users.noreply.github.com>
2017-12-30 18:14:51 +0800

    Create trasysdemo.cpp

---
+++ trasysdemo.cpp
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <assert.h>
+
+#include "trasysfile.h"
+
+int main()
+{
+    char fname[] = "bottle.stl";
+
+    tsysASCIILineReader tsReader(fname);
+    assert(tsReader.getErrorNumber() == tsysFile::errSuccess);
+
+    const char * lineString;
+    tsys_ui32t   llen;
+    while( tsReader.forward() != TSYSFILE_ERROR ) {
+        lineString = NULL; llen = 0;
+        tsReader.getline(lineString, llen);
+
+        if( llen > 0 ) {
+        }
+    } //end of while
+
+    while( tsReader.backward() != TSYSFILE_ERROR ) {
+        lineString = NULL; llen = 0;
+        tsReader.getline(lineString, llen);
+
+        if( llen > 0 ) {
+        }
+    } //end of while
+
+    return 0;
+}
