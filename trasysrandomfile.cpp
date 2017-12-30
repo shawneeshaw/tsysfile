87c46446682bc8e78cf291bca08fd0d9e17d7735
shawneeshaw <shawneeshaw@users.noreply.github.com>
2017-12-30 17:14:41 +0800

    Update trasysrandomfile.cpp

--- trasysrandomfile.cpp
+++ trasysrandomfile.cpp
@@ -12,227 +12,228 @@
 //////////////////////////////////////////////////////////////////////////
 tsysRandomFile::tsysRandomFile(const char *name) {
 #ifdef WIN32
-	fd = INVALID_HANDLE_VALUE;
+  fd = INVALID_HANDLE_VALUE;
 #else
-	fd = -1;
-	flags.immediate = false;
+  fd = -1;
+  flags.immediate = false;
 #endif
-	flags.thrown = flags.initial = flags.temp = false;
-	flags.count = 0;
+  flags.thrown = flags.initial = flags.temp = false;
+  flags.count = 0;
 
 #ifdef _WIN32
-    pathname = _strdup(name);
+  pathname = _strdup(name);
 #else
-    pathname = strdup(name);
+  pathname = strdup(name);
 #endif
 
-    errid = errSuccess;
+  errid = errSuccess;
 }
 
 tsysRandomFile::tsysRandomFile(const tsysRandomFile &rf) {
 #ifdef WIN32
-    HANDLE pidHandle = ::GetCurrentProcess();
-	HANDLE dupHandle;
+  HANDLE pidHandle = ::GetCurrentProcess();
+  HANDLE dupHandle;
 
-	if(rf.fd != INVALID_HANDLE_VALUE) {
-        if(!::DuplicateHandle(pidHandle, rf.fd, pidHandle, &dupHandle, 0, FALSE, DUPLICATE_SAME_ACCESS))
-			fd = INVALID_HANDLE_VALUE;
-		else
-			fd = dupHandle;
-	}
-	else
-		fd = INVALID_HANDLE_VALUE;
+  if(rf.fd != INVALID_HANDLE_VALUE) {
+    if(!::DuplicateHandle(pidHandle, rf.fd, pidHandle, &dupHandle, 0, FALSE, DUPLICATE_SAME_ACCESS))
+      fd = INVALID_HANDLE_VALUE;
+    else
+      fd = dupHandle;
+  }
+  else
+    fd = INVALID_HANDLE_VALUE;
 
 #else
-	if(rf.fd > -1)
-		fd = dup(rf.fd);
-	else
-		fd = -1;
+  if(rf.fd > -1)
+    fd = dup(rf.fd);
+  else
+    fd = -1;
 
 #endif
 
-	flags = rf.flags;
-	flags.count = 0;
+  flags = rf.flags;
+  flags.count = 0;
 
-    if(rf.pathname) {
-        if(pathname) free(pathname);
+  if(rf.pathname) {
+     if(pathname) free(pathname);
 #ifdef _WIN32
-        pathname = _strdup(rf.pathname);
+     pathname = _strdup(rf.pathname);
 #else
-        pathname = strdup(rf.pathname);
+     pathname = strdup(rf.pathname);
 #endif
-    } else
-		pathname = NULL;
+  } else {
+     pathname = NULL;
+  }
 
-    errid = errSuccess;
+  errid = errSuccess;
 }
 
 tsysRandomFile::~tsysRandomFile()
 {
-	final();
+  final();
 }
 
 tsysFile::Error tsysRandomFile::restart(void)
 {
-	return errOpenFailed;
+  return errOpenFailed;
 }
 
 tsysFile::Attr tsysRandomFile::initialize(void)
 {
-	return attrPublic;
+  return attrPublic;
 }
 
 void tsysRandomFile::final(void)
 {
 #ifdef WIN32
-	if(fd != INVALID_HANDLE_VALUE) {
-        ::CloseHandle(fd);
-        if(flags.temp && pathname) ::DeleteFile(pathname);
-	}
+  if(fd != INVALID_HANDLE_VALUE) {
+     ::CloseHandle(fd);
+     if(flags.temp && pathname) ::DeleteFile(pathname);
+  }
 
 #else
-	if(fd > -1) {
-		close(fd);
-		if(flags.temp && pathname) remove(pathname);
-	}
+  if(fd > -1) {
+    close(fd);
+    if(flags.temp && pathname) remove(pathname);
+  }
 #endif
 
-	if(pathname) {
-		free(pathname);
-		pathname = NULL;
-	}
+  if(pathname) {
+    free(pathname);
+    pathname = NULL;
+  }
 
 #ifdef WIN32
-	fd = INVALID_HANDLE_VALUE;
+  fd = INVALID_HANDLE_VALUE;
 #else
-	fd = -1;
+  fd = -1;
 #endif
-	flags.count   = 0;
-	flags.initial = false;
+  flags.count   = 0;
+  flags.initial = false;
 }
 
 tsysRandomFile::Error tsysRandomFile::error(Error id, char *str)
 {
-	errstr = str;
-	errid = id;
-	if(!flags.thrown) {
-		flags.thrown = true;
-	}
-	return id;
+  errstr = str;
+  errid = id;
+  if(!flags.thrown) {
+    flags.thrown = true;
+  }
+  return id;
 }
 
 bool tsysRandomFile::initial(void)
 {
-	bool init;
+  bool init;
 
 #ifdef WIN32
-    if(fd == INVALID_HANDLE_VALUE) return false;
+  if(fd == INVALID_HANDLE_VALUE) return false;
 #else
-    if(fd < 0) return false;
+  if(fd < 0) return false;
 #endif
 
-    init = flags.initial;
-	flags.initial = false;
+  init = flags.initial;
+  flags.initial = false;
 
-	if(!init) return false;
+  if(!init) return false;
 
 #ifdef WIN32
-	Attr access = initialize();
-	if(access == attrInvalid) {
-        ::CloseHandle(fd);
-        if(pathname) ::DeleteFile(pathname);
+  Attr access = initialize();
+  if(access == attrInvalid) {
+    ::CloseHandle(fd);
+    if(pathname) ::DeleteFile(pathname);
 
-		fd = INVALID_HANDLE_VALUE;
-		error(errInitFailed);
-		return false;
-	}
+    fd = INVALID_HANDLE_VALUE;
+    error(errInitFailed);
+    return false;
+  }
 
 #else
-	int mode = (int)initialize();
-	if(!mode) {
-		close(fd);
-        fd = -1;
-		if(pathname) remove(pathname);
+  int mode = (int)initialize();
+  if(!mode) {
+    close(fd);
+    fd = -1;
+    if(pathname) remove(pathname);
 
-		error(errInitFailed);
-		return false;
-	}
-	fchmod(fd, mode);
+    error(errInitFailed);
+    return false;
+  }
+  fchmod(fd, mode);
 #endif
 
-	return init;
+  return init;
 }
 
 #ifndef WIN32
 tsysRandomFile::Error tsysRandomFile::setCompletion(Complete mode)
 {
-	long flag = fcntl(fd, F_GETFL);
+  long flag = fcntl(fd, F_GETFL);
 
-	if(fd < 0)
-		return errNotOpened;
+  if(fd < 0)
+    return errNotOpened;
 
-	flags.immediate = false;
+  flags.immediate = false;
 #ifdef O_SYNC
-	flag &= ~(O_SYNC | O_NDELAY);
+  flag &= ~(O_SYNC | O_NDELAY);
 #else
-	flag &= ~O_NDELAY;
+  flag &= ~O_NDELAY;
 #endif
-	switch(mode) {
-	case completionImmediate:
+  switch(mode) {
+  case completionImmediate:
 #ifdef O_SYNC
-		flag |= O_SYNC;
+    flag |= O_SYNC;
 #endif
-		flags.immediate = true;
-		break;
+    flags.immediate = true;
+    break;
 
-	case completionDelayed:
-		flag |= O_NDELAY;
+  case completionDelayed:
+    flag |= O_NDELAY;
 
-	case completionDeferred:
-		break;
-	}
-	fcntl(fd, F_SETFL, flag);
-	return errSuccess;
+  case completionDeferred:
+    break;
+  }
+  fcntl(fd, F_SETFL, flag);
+  return errSuccess;
 }
 #endif
 
 fsize_64t tsysRandomFile::length(void)
 {
 #ifdef WIN32
-	if(!fd)
+  if(!fd)
 #else
-	if(fd < 0)
+  if(fd < 0)
 #endif
     return 0;
 
-    fsize_64t sz = 0;
+  fsize_64t sz = 0;
 #ifdef WIN32
-	DWORD dwLo = 0l, dwHi = 0l;
+  DWORD dwLo = 0l, dwHi = 0l;
 
-    dwLo = ::GetFileSize(fd, &dwHi);
-    if(dwLo == INVALID_FILE_SIZE && GetLastError() != NO_ERROR){
-        sz = 0;
-    }else{
-        sz = MAKEINT64(dwHi, dwLo);
-    }
+  dwLo = ::GetFileSize(fd, &dwHi);
+  if(dwLo == INVALID_FILE_SIZE && GetLastError() != NO_ERROR){
+     sz = 0;
+  }else{
+     sz = MAKEINT64(dwHi, dwLo);
+  }
 
 #else
-	lseek(fd, (off_t)0, SEEK_SET);
-	off_t pos = lseek(fd, (off_t)0, SEEK_CUR);
-	off_t eof = lseek(fd, (off_t)0, SEEK_END);
-    sz = (fsize_64t)eof;
+  lseek(fd, (off_t)0, SEEK_SET);
+  off_t pos = lseek(fd, (off_t)0, SEEK_CUR);
+  off_t eof = lseek(fd, (off_t)0, SEEK_END);
+  sz = (fsize_64t)eof;
 #endif
-	return sz;
+  return sz;
 }
 
 bool tsysRandomFile::operator!(void)
 {
 #ifdef WIN32
-	return (fd == INVALID_HANDLE_VALUE);
+  return (fd == INVALID_HANDLE_VALUE);
 #else
-	if(fd < 0)
-		return true;
+  if(fd < 0)
+    return true;
 
-	return false;
+  return false;
 #endif
 }
