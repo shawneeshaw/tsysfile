c67b9e3e71b1792389ded23aacdc8bf866a2969a
shawneeshaw <shawneeshaw@users.noreply.github.com>
2017-12-30 16:31:25 +0800

    Create trasysfileutil.h

---
+++ trasysfileutil.h
@@ -0,0 +1,21 @@
+#ifndef	TSYS_FILEUTIL_H
+#define	TSYS_FILEUTIL_H
+
+
+void *quick_mem_find    (const void    *pattern,  long pl,
+                         const void    *source,   long sl,
+                         int shift_rebuild_flag, unsigned int *shift);
+
+void *quick_mem_find_nc (const void    *pattern,  long pl,
+                         const void    *source,   long sl,
+                         int shift_rebuild_flag, unsigned int *shift);
+
+void *quick_mem_rfind   (const void    *pattern,  long pl,
+                         const void    *source,   long sl,
+                         int shift_rebuild_flag, unsigned int *shift);
+
+void *quick_mem_rfind_nc(const void    *pattern,  long pl,
+                         const void    *source,   long sl,
+                         int shift_rebuild_flag, unsigned int *shift);
+
+#endif //TSYS_FILEUTIL_H
