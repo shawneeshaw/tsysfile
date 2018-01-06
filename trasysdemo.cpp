3cb2ba5f2eeb7d1c662bceeaf111b779f5d26a86
shawneeshaw <shawneeshaw@users.noreply.github.com>
2018-01-06 17:09:56 +0800

    Update trasysdemo.cpp

--- trasysdemo.cpp
+++ trasysdemo.cpp
@@ -5,29 +5,26 @@
 
 int main()
 {
-    tsysASCIILineReader tsReader("bottle.stl");
+    char fname[] = "bottle.stl";
+
+    tsysASCIILineReader tsReader(fname);
     assert(tsReader.getErrorNumber() == tsysFile::errSuccess);
 
     const char * lineString;
     tsys_ui32t   llen;
-    
-    //traverse file line by line from the beginning to the ending
-    while( tsReader.forward() != TSYSFILE_ERROR ) {
+    while( tsReader.Forward() != TSYSFILE_ERROR ) {
         lineString = NULL; llen = 0;
         tsReader.getline(lineString, llen);
 
         if( llen > 0 ) {
-            //do your actions...
         }
     } //end of while
 
-    //traverse file line by line from the ending to the beginning
-    while( tsReader.backward() != TSYSFILE_ERROR ) {
+    while( tsReader.Backward() != TSYSFILE_ERROR ) {
         lineString = NULL; llen = 0;
         tsReader.getline(lineString, llen);
 
         if( llen > 0 ) {
-            //do your actions...
         }
     } //end of while
 
